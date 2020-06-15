f8f3b33a91ed3d35ed86ed9dc470afad4bb23a8e
bisarnacki <66859995+bisarnacki@users.noreply.github.com>
2020-06-15 06:14:38 +0200

    created app.vue / api / login

---
+++ app.vue
@@ -0,0 +1,36 @@
+// App.vue
+<template>
+  <div>
+    <AppHeader :user="user" @logout="logOut" @login="logIn" />
+    <HomePage :user="user" />
+  </div>
+</template>
+
+<script>
+import { getUser } from './api'
+
+export default {
+  data () {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+   logOut () {
+     this.user = null
+   },
+   logIn () {
+     this.user = await getUser()
+   }
+  } 
+}
+</script>
+
+
+/* Router-view
+<template>
+  <div>
+    <AppHeader :user="user" @logout="logOut" @login="logIn" />
+    <router-view :user="user" />
+  </div> */
+</template>
